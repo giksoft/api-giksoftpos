0aac7e332adc5199dd8c480e3845dd44573ddaf8
giksoft <giksoft.technologies.soporte@gmail.com>
2024-09-13 21:18:28 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,9 +7,9 @@ module.exports = {
       secret: process.env.JET_SECRET || 'notasecreta'
     },
     mysql: {
-        host: process.env.MYSQL_HOST || '149.56.140.45',
-        user: process.env.MYSQL_USER || 'braulio',
-        password: process.env.MYSQL_PASSWORD || 'Octubre90!',
-        database: process.env.MYSQL_DB || 'lapopular',
+        host: process.env.MYSQL_HOST || 'localhost',
+        user: process.env.MYSQL_USER || 'giksoft_pos_user_master',
+        password: process.env.MYSQL_PASSWORD || '943167qwerty!',
+        database: process.env.MYSQL_DB || 'giksoft_pos_master',
     }
 }
